3673210db685a0e8ea0fab4972bcbad986bdbed8
qws <785260172@qq.com>
2018-07-09 14:21:30 +0800

    erueak

---
+++ erueak/src/main/java/com/qws/erueak/SpringErueakApplication.java
@@ -0,0 +1,18 @@
+package com.qws.erueak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author qiwenshuai
+ * @description
+ * @since 18-7-9 14:05 by jdk 1.8
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringErueakApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(SpringErueakApplication.class, args);
+        }
+}
